5d7c746fbc48bf596f8587eb214c23aa8c4352ec
user-pc <kswksw0322@naver.com>
2025-04-29 23:30:37 +0900

    시험

---
+++ src/main/java/org/example/Eexam06.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Eexam06 {
+    public static void main(String[] args){
+        int sum=0;
+
+        for(int i=100; i < 300; i++ ){
+            if(i%5==0||i%7==0){
+                sum+=i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
